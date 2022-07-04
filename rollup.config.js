7d5e86e25973d1fe6803047c2d26b5b1f00eaa3b
Tom Weatherhead <tomw@2hrd4u.org>
2022-07-03 23:21:47 -0600

    Dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,43 @@
 // thaw-http-json-client-angular/rollup.config.js
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { terser } = require('rollup-plugin-terser');
+/**
+ * Copyright (c) Tom Weatherhead. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
 
-export default [
-	{
-		input: './dist/types/main.js',
-		output: [
-			{
-				file: 'dist/thaw-http-json-client-angular.cjs.js',
-				format: 'cjs',
-				exports: 'named'
-			},
-			{
-				file: 'dist/thaw-http-json-client-angular.esm.js',
-				format: 'es',
-				compact: true,
-				plugins: [terser()]
-			},
-			{
-				file: 'dist/thaw-http-json-client-angular.js',
-				name: 'thaw-http-json-client-angular',
-				format: 'umd',
-				compact: true,
-				plugins: [terser()]
-			}
-		]
-	}
-];
+'use strict';
+
+// import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: './dist/types/main.js',
+	output: [
+		// {
+		// 	file: 'dist/thaw-http-json-client-angular.cjs.js',
+		// 	format: 'cjs',
+		// 	exports: 'named'
+		// },
+		{
+			file: 'dist/thaw-http-json-client-angular.esm.js',
+			format: 'es',
+			esModule: true,
+			compact: true // ,
+			// plugins: [terser()]
+		} // ,
+		// {
+		// 	file: 'dist/thaw-http-json-client-angular.js',
+		// 	name: 'thaw-http-json-client-angular',
+		// 	format: 'umd',
+		// 	compact: true // ,
+		// 	// plugins: [terser()]
+		// }
+	],
+	context: 'this',
+	// See https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency :
+	external: ['@angular/common/http', 'rxjs', 'rxjs/operators'],
+	// plugins: [nodeResolve(), terser()]
+	plugins: [terser()]
+};
